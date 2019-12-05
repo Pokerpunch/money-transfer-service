834f3ca15ffe4ceae43ee09ffeaa5057e8d42cca
pb <pb@email.com>
2019-12-05 19:04:06 +0200

    Add mappings and table definitions for Accounts and Transfers

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -6,3 +6,22 @@ CREATE TABLE IF NOT EXISTS `users` (
     `lastName` VARCHAR NOT NULL,
     `dateCreated` TIMESTAMP NOT NULL
 );
+
+CREATE SEQUENCE `account_sequence` INCREMENT BY 1;
+CREATE TABLE IF NOT EXISTS `accounts` (
+    `id` BIGINT DEFAULT `account_sequence`.nextval PRIMARY KEY,
+    `version` BIGINT,
+    `accountOwnerId` BIGINT NOT NULL,
+    `balanceInMinor` BIGINT NOT NULL,
+    `dateCreated` TIMESTAMP NOT NULL,
+    `dateUpdated` TIMESTAMP NOT NULL
+);
+
+CREATE SEQUENCE `transfer_sequence` INCREMENT BY 1;
+CREATE TABLE IF NOT EXISTS `transfers` (
+    `id` BIGINT DEFAULT `transfer_sequence`.nextval PRIMARY KEY,
+    `originAccountId` BIGINT NOT NULL,
+    `destinationAccountId` BIGINT NOT NULL,
+    `amountInMinor` BIGINT NOT NULL,
+    `dateCreated` TIMESTAMP NOT NULL
+);
