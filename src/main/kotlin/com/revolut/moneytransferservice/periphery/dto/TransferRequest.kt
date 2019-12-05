cb20354f31ee63494c91bd8c555ec26702cb8318
pb <pb@email.com>
2019-12-05 19:26:21 +0200

    Rename amountMinor to amountInMinor

--- src/main/kotlin/com/revolut/moneytransferservice/periphery/dto/TransferRequest.kt
+++ src/main/kotlin/com/revolut/moneytransferservice/periphery/dto/TransferRequest.kt
@@ -3,5 +3,5 @@ package com.revolut.moneytransferservice.periphery.dto
 data class TransferRequest(
     val originAccountId: Long,
     val destinationAccountId: Long,
-    val amountMinor: Long
+    val amountInMinor: Long
 )
